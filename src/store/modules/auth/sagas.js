857817f7b3dd4eda841d0c90bb9b801a31fc98e2
Danilo <h2danilo@hotmail.com>
2021-02-03 18:22:03 -0300

    Cadastro na aplicacao

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -32,6 +32,26 @@ export function* signIn({ payload }) {
   }
 }
 
+export function* signUp({ payload }) {
+  try {
+    const { name, email, password } = payload;
+
+    // yield = como se fosse o await do generator, td que vier após irá aguardar execucao.
+    // call =  responsavel por chamar metodos. que sao assincronos e que retornar promisses.
+    yield call(api.post, 'users', {
+      name,
+      email,
+      password,
+      provider: true,
+    });
+
+    history.push('/');
+  } catch (err) {
+    toast.error('Falha no cadastro, verifique seu dados!');
+    yield put(signFailure());
+  }
+}
+
 // all => cadastrar varios listners, que ficar ouvindo qdo uma action for disparada para disparar essa acao
 // takeLatest => se usuario clicar uma vez no botao para adicionar no carrinho e logo em seguida clicar novamente
 // antes da chamada api finalizar, o saga irá descartar a primeira chamada e executar a atual (adicionando uma vez só no carrinho).
@@ -39,4 +59,5 @@ export function* signIn({ payload }) {
 export default all([
   // takeLatest => toda vez que ouvir requisicao SIGN_IN_REQUEST chama a funcao "signIn"
   takeLatest('@auth/SIGN_IN_REQUEST', signIn),
+  takeLatest('@auth/SIGN_UP_REQUEST', signUp),
 ]);
