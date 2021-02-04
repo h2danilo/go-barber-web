7bd2eaf9dea8ea8e01199c32fd717dfaa8f3a309
Danilo <h2danilo@hotmail.com>
2021-02-04 14:15:00 -0300

    Pagina de Perfil

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,9 +1,38 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { Form, Input } from '@rocketseat/unform';
 
-// import { Container } from './styles';
+import { Container } from './styles';
 
-function Profile() {
-  return <h1>Prof</h1>;
+export default function Profile() {
+  const profile = useSelector((state) => state.user.profile);
+
+  function handleSubmit(data) {}
+
+  return (
+    <Container>
+      <Form initialData={profile} onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Nome Completo" />
+        <Input name="email" type="email" placeholder="Seu endereço de email" />
+
+        <hr />
+
+        <Input
+          name="oldpassword"
+          type="password"
+          placeholder="Sua senha atual"
+        />
+        <Input name="password" type="password" placeholder="Nova senha" />
+        <Input
+          name="confirmpassword"
+          type="password"
+          placeholder="Confirmação de senha"
+        />
+
+        <button type="submit">Atualizar perfil</button>
+      </Form>
+
+      <button type="button">Sair do GoBarber</button>
+    </Container>
+  );
 }
-
-export default Profile;
