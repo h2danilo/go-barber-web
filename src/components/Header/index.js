68306d2ff88c1b471cfeab81e1b1407a6a946c68
Danilo <h2danilo@hotmail.com>
2021-02-04 10:34:26 -0300

    Configurando Header

---
+++ src/components/Header/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import logo from '~/assets/logo_50.svg';
+
+import { Container, Content, Profile } from './styles';
+
+function Header() {
+  return (
+    <Container>
+      <Content>
+        <nav>
+          <img src={logo} alt="goBarber" />
+          <Link to="/dashboard">HOME</Link>
+        </nav>
+
+        <aside>
+          <Profile>
+            <div>
+              <strong>Danilo Valim</strong>
+              <Link to="/profile">Meu Perfil</Link>
+            </div>
+          </Profile>
+          <img
+            src="https://avatars.dicebear.com/4.5/api/bottts/danilovalim.svg?w=50&h=50"
+            alt="Danilo valim"
+          />
+        </aside>
+      </Content>
+    </Container>
+  );
+}
+
+export default Header;
--- src/pages/_layouts/default/index.js
+++ src/pages/_layouts/default/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import Header from '~/components/Header';
 // ~ => sempre que comacar com ~ volta p pasta raiz definida no arquivo config-overrides.js (src)
 import { Wrapper } from './styles';
 
 function DefaultLayout({ children }) {
-  return <Wrapper>{children}</Wrapper>;
+  return (
+    <Wrapper>
+      <Header />
+      {children}
+    </Wrapper>
+  );
 }
 
 DefaultLayout.propTypes = {
