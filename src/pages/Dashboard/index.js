0c06676e764c4a30f1ad0f52c5b1621698ca7a9c
Danilo <h2danilo@hotmail.com>
2021-02-05 11:38:45 -0300

    Estilizacao do Dashboard

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,11 +1,42 @@
 import React from 'react';
-import api from '~/services/api';
-// import { Container } from './styles';
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
+// import api from '~/services/api';
+
+import { Container, Time } from './styles';
 
 function Dashboard() {
-  api.get('appointments');
+  return (
+    <Container>
+      <header>
+        <button type="button">
+          <MdChevronLeft size={36} color="#fff" />
+        </button>
+        <strong>31 de maio</strong>
+        <button type="button">
+          <MdChevronRight size={36} color="#fff" />
+        </button>
+      </header>
 
-  return <h1>Dash</h1>;
+      <ul>
+        <Time past>
+          <strong>08:30</strong>
+          <span>Danilo Valim</span>
+        </Time>
+        <Time available>
+          <strong>09:00</strong>
+          <span>Em Aberto</span>
+        </Time>
+        <Time>
+          <strong>10:30</strong>
+          <span>Danilo Valim</span>
+        </Time>
+        <Time>
+          <strong>11:00</strong>
+          <span>Danilo Valim</span>
+        </Time>
+      </ul>
+    </Container>
+  );
 }
 
 export default Dashboard;
